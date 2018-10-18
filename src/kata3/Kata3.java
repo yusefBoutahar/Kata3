4ae2f5aa35418b56d3d35b61a3389a987658ecbd
Usuario <Usuario@10.22.148.82>
2018-10-18 11:18:14 +0100

    Version 1

---
+++ src/kata3/Kata3.java
@@ -0,0 +1,10 @@
+package kata3;
+
+public class Kata3 {
+
+    public static void main(String[] args) {
+        HistogramDisplay histo = new HistogramDisplay();
+        histo.execute();
+    }
+    
+}
